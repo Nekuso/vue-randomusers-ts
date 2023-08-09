fc6a22713b2964451fd7cd1b490fefc991da7da7
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-09 11:22:18 +0800

    Updated

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -3,13 +3,11 @@ import DarkModeButton from '@/components/dark-mode-button.vue'
 </script>
 
 <template>
-  <div class="content w-full pt-9 flex justify-between">
+  <nav class="content w-full pt-9 flex justify-between">
     <a href="/users" class="flex items-center">
-      <h1 class="text-4xl font-bold dark:text-slate-100 transition-colors duration-500">
-        Nekuso
-      </h1>
+      <h1 class="text-4xl font-bold dark:text-slate-100 transition-colors duration-500">Nekuso</h1>
     </a>
 
     <DarkModeButton class="scale-75" />
-  </div>
+  </nav>
 </template>
