38fd14b474f1dc9d5bba85b68f3c8f18eac2ee66
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2023-08-09 14:54:50 +0800

    Updated

--- src/components/content-list.vue
+++ src/components/content-list.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { User } from '@/types/user'
 import type { PropType } from 'vue'
-import ContentListItem from './content-list-item.vue';
+import ContentListItem from './content-list-item.vue'
 defineProps({
   paginatedUsers: {
     type: Array as PropType<User[]>,
@@ -17,9 +17,7 @@ defineProps({
         class="bg-white dark:bg-slate-900 relative overflow-x-auto shadow-md sm:rounded-lg h-full transition-colors duration-500"
       >
         <table
-          class="w-full text-sm text-left text-gray-500 dark:text-gray-400 transition-colors duration-500 table-fixed 
-            justify-center align-center
-          "
+          class="w-full text-sm text-left text-gray-500 dark:text-gray-400 transition-colors duration-500 table-fixed justify-center align-center"
         >
           <thead
             class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 transition-colors duration-500"
@@ -28,11 +26,10 @@ defineProps({
               <th scope="col" class="px-6 py-3 w-[65%]">Name</th>
               <th scope="col" class="px-6 py-3">Location</th>
               <th scope="col" class="px-6 py-3">Gender</th>
-              <!-- <th scope="col" class="px-6 py-3">Action</th> -->
             </tr>
           </thead>
           <tbody>
-            <ContentListItem :paginatedUsers="paginatedUsers"/> 
+            <ContentListItem :paginatedUsers="paginatedUsers" />
           </tbody>
         </table>
       </div>
